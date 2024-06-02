6109dfce500422e8e1dbfdf2ac83c674165018f2
ballooner <26182964+ballooner@users.noreply.github.com>
2024-06-02 09:30:08 -0400

    Added requirements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, numToRemove) {
+    for (i = 1; i < arguments.length; i++)
+    {
+        //Get the index of the number
+        indexOfNum = inputArray.indexOf(arguments[i]);
+        //Only remove if the number is present
+        if (indexOfNum != -1)
+            inputArray.splice(indexOfNum, 1);
+        //Make sure to remove every instance of the variable
+        if (inputArray.indexOf(arguments[i]) != -1)
+            removeFromArray(inputArray, arguments[i]);
+    }
+
+    return inputArray;
 };
 
 // Do not edit below this line
