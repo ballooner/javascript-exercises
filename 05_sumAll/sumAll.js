17a0b1306dcfb624d6bf23dd4fea0476ffe19396
ballooner <26182964+ballooner@users.noreply.github.com>
2024-06-02 09:45:55 -0400

    Added requirements

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstInput, secondInput) {
+    if (!((typeof firstInput == "number" && typeof secondInput == "number") && (firstInput > 0 && secondInput > 0)))
+        return "ERROR";
 
+    sum = 0;
+
+    startNum = firstInput < secondInput ? firstInput : secondInput;
+    endNum = firstInput > secondInput ? firstInput : secondInput;
+
+    for (i = startNum; i <= endNum; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
