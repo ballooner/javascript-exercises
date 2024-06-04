3d44555b6bbdf921087f0cce2370b04e4471e302
ballooner <26182964+ballooner@users.noreply.github.com>
2024-06-04 17:08:29 -0400

    Added requirements

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    fibonacciArray = [0, 1, 1];
 
+    for (i = 2; i < num; i++)
+        {
+            prevNum = fibonacciArray.at(i - 1);
+            currNum = fibonacciArray.at(i);
+
+            fibonacciArray.push(prevNum += currNum);
+        }
+
+    return num >= 0 ? fibonacciArray.at(num) : "OOPS";
 };
 
 // Do not edit below this line
