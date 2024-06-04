ad1336c83430dc8ac3158a34af584ce100d21c0a
ballooner <26182964+ballooner@users.noreply.github.com>
2024-06-04 16:58:09 -0400

    Added requirements

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    let newString = string.toLowerCase().match(/[\w]/g);
+    
+    return newString.join("") === newString.reverse().join("");
 };
 
 // Do not edit below this line
