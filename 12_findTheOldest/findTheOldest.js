686670784ef7881e38f9547e51a0c48e67e57e19
ballooner <26182964+ballooner@users.noreply.github.com>
2024-06-04 19:15:57 -0400

    Added requirements

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    let oldestPerson = peopleArray.reduce( (oldestPerson, currentPerson) => {
+        let currentBirthDate = currentPerson.yearOfBirth;
+        let currentDeathDate = currentPerson.yearOfDeath === undefined ? new Date().getFullYear() : currentPerson.yearOfDeath;
+        let oldestBirthDate = oldestPerson.yearOfBirth;
+        let oldestDeathDate = oldestPerson.yearOfDeath === undefined ? new Date().getFullYear() : oldestPerson.yearOfDeath;
 
+        let age = currentDeathDate - currentBirthDate;
+        let oldestAge = oldestDeathDate - oldestBirthDate;
+
+        if (age > oldestAge)
+            return currentPerson;
+
+        return oldestPerson;
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
